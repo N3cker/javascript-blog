c20637aba74c64a25e76b7ba10e1beb14a6359ee
Mateusz Piasecki <piaszuu@gmail.com>
2019-10-20 05:31:16 +0200

    Finish function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -1,52 +1,86 @@
 'use strict';
-/*
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  });
-  */
+
 const titleClickHandler = function(){
   const clickedElement = this;
   event.preventDefault();
   console.log('Link was clicked!');
-  console.log(event)
+  console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /* [DONE] add class 'active' to the clicked link */
 
-clickedElement.classList.add('active')
+  clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
 
-const activeArticles = document.querySelectorAll('.posts article.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* [DONE] get 'href' attribute from the clicked link */
 
-  const hrefAttribute = clickedElement.getAttribute('href');
+    const hrefAttribute = clickedElement.getAttribute('href');
 
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-  const selectedArticle = document.querySelector(hrefAttribute);
+    const selectedArticle = document.querySelector(hrefAttribute);
 
   /* [DONE] add class 'active' to the correct article */
 
-  selectedArticle.classList.add('active');
-
+    selectedArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector).innerHTML = '';
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  
+  for(let article of articles){
+
+    /* get the article id */
+
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    
+    /* insert link into titleList */
+    
+    document.querySelector('.titles').innerHTML += linkHTML
+    
+  }
+  const links = document.querySelectorAll('.titles a');
+console.log(links)
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+}
+
+generateTitleLinks();
